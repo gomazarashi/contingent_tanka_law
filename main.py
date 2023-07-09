8ac0f1a373fea2a7a82fee2b88607f8648e071dd
gomazarashi <Seto42357@gmail.com>
2023-07-09 12:37:17 +0900

    [add]convert_to_katakana関数を追加

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import re
 from xml.etree import ElementTree
 from functools import lru_cache
 import jamorasep as jms
+import pykakasi
 
 #法令番号のリストを作成する関数
 def make_law_number_list(category=1):
@@ -20,6 +21,13 @@ def get_law_text(law_number):
     root = ElementTree.fromstring(response.content.decode(encoding="utf-8"))
     contents = [e.text.strip() for e in root.iter() if e.text]
     return [text.replace('\u3000', '') for text in contents if text] #全角スペースを削除
+
+#与えられた文字列をカタカナに変換する関数
+def convert_to_katakana(text):
+    kks = pykakasi.kakasi()
+    result = kks.convert(text)
+    return ''.join([item['kana'] for item in result])
+
 
 #与えられたカタカナの文字列を5,7,5に分割する関数
 def create_haiku(text):
